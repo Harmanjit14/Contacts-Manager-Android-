6a0e34b4d4c019e9e03c44441d7044b9f3d2b0e1
Harmanjit Singh <harmanjit140500@gmail.com>
2020-06-14 02:50:19 +0530

    created class viewholder and adapter

---
+++ app/src/main/java/com/example/easycontacts/RecyclerAdapter.java
@@ -0,0 +1,28 @@
+package com.example.easycontacts;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<ViewHolder>{
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.single_contact_view,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/example/easycontacts/ViewHolder.java
@@ -0,0 +1,11 @@
+package com.example.easycontacts;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+    public ViewHolder(View v){
+        super(v);
+    }
+}
